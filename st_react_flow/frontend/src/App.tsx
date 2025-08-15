7029ef1d4a4acb41489f1fe60ab7c1d423faff50
j-w-matlock <matlock.john.w@icloud.com>
2025-08-15 10:25:59 -0700

    Remove canvas interactions in favor of sidebar inputs

--- st_react_flow/frontend/src/App.tsx
+++ st_react_flow/frontend/src/App.tsx
@@ -3,19 +3,14 @@ import ReactFlow, {
   Background,
   Controls,
   MiniMap,
-  addEdge,
-  MarkerType,
-  Connection,
   Node,
   Edge,
   NodeChange,
   EdgeChange,
   applyNodeChanges,
   applyEdgeChanges,
-  ReactFlowInstance,
 } from "reactflow";
 import "reactflow/dist/style.css";
-import { nanoid } from "nanoid";
 import { Streamlit, StreamlitComponentBase, withStreamlitConnection } from "streamlit-component-lib";
 import type { NodeData, EdgeData } from "./types";
 
@@ -24,8 +19,6 @@ class App extends StreamlitComponentBase<any> {
     nodes: (this.props.args?.value?.nodes ?? []) as Node<NodeData>[],
     edges: (this.props.args?.value?.edges ?? []) as Edge<EdgeData>[],
   };
-
-  reactFlowInstance: ReactFlowInstance | null = null;
 
   componentDidMount() {
     Streamlit.setFrameHeight();
@@ -42,33 +35,13 @@ class App extends StreamlitComponentBase<any> {
   }
 
   onNodesChange = (changes: NodeChange[]) => {
-    this.setState((s: any) => ({ nodes: applyNodeChanges(changes, s.nodes) }));
+    const filtered = changes.filter((c) => c.type !== "remove");
+    this.setState((s: any) => ({ nodes: applyNodeChanges(filtered, s.nodes) }));
   };
 
   onEdgesChange = (changes: EdgeChange[]) => {
-    this.setState((s: any) => ({ edges: applyEdgeChanges(changes, s.edges) }));
-  };
-
-  onConnect = (conn: Connection) => {
-    const e: Edge<EdgeData> = {
-      ...conn,
-      id: nanoid(),
-      markerEnd: { type: MarkerType.ArrowClosed },
-    } as any;
-    this.setState((s: any) => ({ edges: addEdge(e, s.edges) }));
-  };
-
-  onPaneContextMenu = (event: React.MouseEvent) => {
-    event.preventDefault();
-    if (!this.reactFlowInstance) return;
-    const position = this.reactFlowInstance.project({ x: event.clientX, y: event.clientY });
-    const node: Node<NodeData> = {
-      id: nanoid(),
-      type: "default",
-      position,
-      data: { label: "New node" },
-    };
-    this.setState((s: any) => ({ nodes: [...s.nodes, node] }));
+    const filtered = changes.filter((c) => c.type !== "remove");
+    this.setState((s: any) => ({ edges: applyEdgeChanges(filtered, s.edges) }));
   };
 
   render() {
@@ -81,9 +54,6 @@ class App extends StreamlitComponentBase<any> {
           edges={edges}
           onNodesChange={this.onNodesChange}
           onEdgesChange={this.onEdgesChange}
-          onConnect={this.onConnect}
-          onInit={(rf) => (this.reactFlowInstance = rf)}
-          onPaneContextMenu={this.onPaneContextMenu}
           fitView
         >
           <MiniMap />
